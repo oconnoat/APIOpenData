ae6e06b5691f0a050961a64fc7b647970499da1c
Alex O'Connor <Alex.OConnor@scss.tcd.ie>
2014-09-17 08:59:36 +0100

    layout done, need to re-download poster picture, take a screenshot. fix d3

--- js/diagrams.js
+++ js/diagrams.js
@@ -3,3 +3,5 @@ var rdfGraph = d3.select('#RDFGraph').append('svg');
 rdfGraph.attr('height', '100%').attr('width', '50%').attr('pointer-events','auto');
 
 rdfGraph.append('svg:rect').attr('width','50px').attr('height','100px').attr('fill','#ff0000');
+rdfGraph.append('svg:rect').attr('x','50').attr('y','50').attr('width','50px').attr('height','100px').attr('fill','#ffff00');
+rdfGraph.append('svg:rect').attr('x','50').attr('y','150').attr('width','50px').attr('height','100px').attr('fill','#ff00FF');
