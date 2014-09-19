aa1b08a84f7f859be711c3bde6e804e35b259dee
Alex O'Connor <Alex.OConnor@scss.tcd.ie>
2014-09-19 23:31:29 +0200

    version draft 1

--- js/diagrams.js
+++
@@ -1,5 +0,0 @@
-var rdfGraph = d3.select('#RDFGraph').append('svg');
-
-rdfGraph.attr('height', '100%').attr('width', '50%').attr('pointer-events','auto').attr('style','border: 1px solid black');
-
-rdfGraph.append('svg:ellipse').attr('cx','5').attr('cy','5').attr('rx','50').attr('ry','25').attr('fill','#ff0000');
